4e621bd5d033eac0a3eb8e3e16c45c2f87c0d1e3
Axel Anaya <axel_anaya@hotmail.com>
2020-12-05 13:23:05 -0600

    se cambio el nombre de la funcion

--- cypress/integration/fillForm/evaluacion_docente.js
+++ cypress/integration/fillForm/evaluacion_docente.js
@@ -1,22 +1,27 @@
 describe('HACIENDO VALUACION DIAGNOSTICO', () => {
 
-  function anwserQuestionary () {
+  function anwserQuestionnaire () {
     cy.get('.btn-primary').first().click()
 
+    //Primer cuestioanrio
     cy.UADYRadioSelect(3,4)
 
-    cy.UADYNextQuestionary()
+    cy.UADYNextQuestionnaire()
 
+    //Segundo cuestionario
     cy.UADYAdjectives(1,1)
 
-    cy.UADYNextQuestionary()
+    cy.UADYNextQuestionnaire()
 
+    //Tercer cuestionario
     cy.UADYRadioSelect(3,3)
 
-    cy.UADYNextQuestionary()
+    cy.UADYNextQuestionnaire()
 
+    //Cuarto cuestionario - parte 1
     cy.UADYRadioSelectYesOrNo('Si')
 
+    //Cuarto cuestionario - parte 2
     cy.UADYFillTextArea()
 
     cy.contains('Guardar').click()
@@ -28,7 +33,7 @@ describe('HACIENDO VALUACION DIAGNOSTICO', () => {
 
     // Entrar al cuestionario
     cy.get('.btn-primary').first().click()
-    anwserQuestionary()
+    anwserQuestionnaire()
     
   })
 })
